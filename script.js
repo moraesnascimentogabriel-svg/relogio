b804b339321729208f98d599c78eaf59a81eae1b
moraesnascimentogabriel-svg <moraes.nascimento.gabriel@escola.pr.gov.br>
2026-06-16 12:41:33 -0300

    update

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ function carregar(){
     var img = window.document.getElementByld('imagem')
 
     var data = new Date()
-    var hora = data getHours()
+    var hora = data.getHours()
     var minutos = data.getMinutes()
 
     msg.innerHTML = `Agora são ${hora}:${minutos}Am`
-      if(hora >= 0 && <12){
+      if(hora >= 0 && hora<12){
         img.src = 'manha.png'
         document.body.style.background = '#ffb703'
     }
     else if (hora >= 12 && hora < 18){
-        mg.src = 'tarde.png'
+        img.src = 'tarde.png'
         document.body.style.background = '#00b4d8'
     }
     else{
         img.src = 'noite.png'
         document.body.style.background = '#000814'
 
-}
+}}
